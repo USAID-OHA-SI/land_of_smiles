f84743ffccd44ecb4414b68efc764cd482ca3e66
tessam30 <tim.essam@gmail.com>
2023-08-01 14:36:16 -0400

    feat: add in day 2 know r

---
+++ Scripts/Day2_session01_getting_to_know_r_exercise_1_know_r.R
@@ -0,0 +1,48 @@
+# Exercise: Day2_session01_getting_to_know_r_exercise_1_know_r
+
+# SETUP -------------------------------------------------------------------
+
+  # Practice loading libraries
+  # Load the following libraries
+  # tidyverse
+  # gagglr
+  # scales
+
+  library()
+  
+
+# STRINGS & NUMERICS ------------------------------------------------------
+
+  # Practice creating strings and numbers in objects
+  your_name <- c("")
+  
+  your_number <- 
+    
+  # Print your name and number to the console
+    print()  
+  
+
+# DATAFRAMES --------------------------------------------------------------
+
+  # Create a new data frame
+  # column X should take the values "E", "F", "G"
+  # and column y should take values 3, 2, 1
+  
+  df <- data.frame(x = c(), y = c())
+  
+  # Practice accessing columns in the data frame
+  df$x
+  
+  
+# CREATE A FUNCTION -------------------------------------------------------
+
+  # Create a greeting function
+  # follow the instructions from the slide
+  greeting <- function(){
+    paste()
+  }
+  
+  # Test it
+  greeting(“”)
+  
+    
