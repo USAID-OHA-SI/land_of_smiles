2fae937f4d07e654434c4e109a64b0be4cabbb02
Aaron Chafetz <achafetz@usaid.gov>
2023-08-03 11:30:00 -0400

    add gdrive upload

--- Scripts/exercise_untidy.R
+++ Scripts/exercise_untidy.R
@@ -17,6 +17,7 @@
   library(patchwork)
   library(ggtext)
   library(openxlsx)
+  library(googledrive)
   
 
 # GLOBAL VARIABLES --------------------------------------------------------
@@ -47,8 +48,11 @@
 
 # EXPORT FOR EXERCISE -----------------------------------------------------
 
-  write.xlsx(df_ex, "Dataout_public/exercise_untidy.xlsx", overwrite = TRUE)
+  write.xlsx(df_ex, "Dataout/exercise_untidy.xlsx", overwrite = TRUE)
 
+  # drive_upload("Dataout/exercise_untidy.xlsx",
+  #              path = as_id("1Dmo7NqtBCbexsaq6XpkN1MZlTn-s2Jkz"),
+  #              type = "spreadsheet")
   
 # VIZ MUNGING -------------------------------------------------------------
 
